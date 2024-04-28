c93a8bec84e6565f101a873fd95f109100756e7f
mrAxekash <aliaxandr_ku@mail.ru>
2024-04-28 22:42:38 +0300

    lesson 2 task 6 finish react-loading-skeleton

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'react-loading-skeleton/dist/skeleton.css'
 import { Decks } from '../features/decks/Decks.tsx'
 import { GlobalError } from './GlobalError/GlobalError.tsx'
 import { useAppSelector } from './store.ts'
@@ -13,6 +14,14 @@ export const App = () => {
       {status === 'loading' && <LinearLoader />}
       <Decks />
       <GlobalError />
+      <footer>
+        Hello world Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello world
+      </footer>
     </div>
   )
 }
